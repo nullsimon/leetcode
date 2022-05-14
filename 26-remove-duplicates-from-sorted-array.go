dc932876ed20fdb061ddbab59edd230430415baa
nullsimon <uuapril@gmail.com>
2022-05-14 09:17:56 +0800

    add 26-remove-duplicates-from-sorted-array.go

---
+++ 26-remove-duplicates-from-sorted-array.go
@@ -0,0 +1,13 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		nums[k] = nums[i]
+		for i < len(nums)-1 && nums[k] == nums[i+1] {
+			i++
+		}
+		k++
+	}
+	return k
+}
