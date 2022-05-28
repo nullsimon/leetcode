d04b52141081c856d2d9d9afa16ca0e0b3ae90e5
nullsimon <uuapril@gmail.com>
2022-05-28 21:36:15 +0800

    fix coins total number

--- algorithms/322-coin-change/322-coin-change_test.go
+++ algorithms/322-coin-change/322-coin-change_test.go
@@ -8,7 +8,7 @@ func TestCoinChange(t *testing.T) {
 
 	var coins = []int{186, 419, 83, 408}
 	var amount = 6249
-	var want = 13
+	var want = 20
 	got := coinChange(coins, amount)
 	if got != want {
 		t.Fatalf(`coinChange func failed: want %d, got %d`, want, got)
