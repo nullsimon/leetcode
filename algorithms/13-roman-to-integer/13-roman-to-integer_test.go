cb33a79e3d5b24f44702c3fa205e371e872a8d9d
nullsimon <uuapril@gmail.com>
2022-05-29 18:13:43 +0800

    fix romantointeger test cases

--- algorithms/13-roman-to-integer/13-roman-to-integer_test.go
+++ algorithms/13-roman-to-integer/13-roman-to-integer_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestRomanToInt(t *testing.T) {
 	input := []string{"III", "I", "IV", "VII"}
-	want := []int{3, 1, 4, 6}
+	want := []int{3, 1, 4, 7}
 	for i := 0; i < len(input); i++ {
 		got := romanToInt(input[i])
 		if got != want[i] {
