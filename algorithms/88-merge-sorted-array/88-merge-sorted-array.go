38d28ffaa51b041add832cac34c06bc29c162fd3
nullsimon <uuapril@gmail.com>
2022-06-06 12:13:03 +0800

    fix 88-merge-sorted-array using reverse order

--- algorithms/88-merge-sorted-array/88-merge-sorted-array.go
+++ algorithms/88-merge-sorted-array/88-merge-sorted-array.go
@@ -1,7 +1,5 @@
 package mergesortedarray
 
-import "fmt"
-
 func merge(nums1 []int, m int, nums2 []int, n int) []int {
 	return merge_version2(nums1, m, nums2, n)
 }
@@ -41,27 +39,25 @@ func merge_version1(nums1 []int, m int, nums2 []int, n int) []int {
 		}
 
 	}
-	fmt.Println(nums3)
 	nums1 = nums3
-	fmt.Println(nums1)
 	return nums1
 }
 
 func merge_version2(nums1 []int, m int, nums2 []int, n int) []int {
-	var n1, n2 int
-	for k := 0; k < m+n; k++ {
-		if n1 < m && n2 < n {
-			if nums1[n1] <= nums2[n2] {
-				n1++
-				continue
+	i := m - 1
+	j := n - 1
+	for k := m + n - 1; k >= 0; k-- {
+		if i >= 0 && j >= 0 {
+			if nums1[i] > nums2[j] {
+				nums1[k] = nums1[i]
+				i--
+			} else if nums1[i] <= nums2[j] {
+				nums1[k] = nums2[j]
+				j--
 			}
-			if nums1[n1] > nums2[n2] {
-				nums3 := nums1[n1 : m+n-1]
-				nums1 = append(nums1[:n1], nums2[n2])
-				nums1 = append(nums1, nums3...)
-				n2++
-				continue
-			}
+		} else if j >= 0 {
+			nums1[k] = nums2[j]
+			j--
 		}
 	}
 	return nums1
