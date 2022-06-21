020b0331a67835564dc55d16b4e72d2ec903dd46
nullsimon <uuapril@gmail.com>
2022-06-21 14:31:07 +0800

    add 35-search-insert-position right bound version

--- algorithms/35-search-insert-position/35-search-insert-position.go
+++ algorithms/35-search-insert-position/35-search-insert-position.go
@@ -2,6 +2,7 @@ package searchinsertposition
 
 func searchInsert(nums []int, target int) int {
 	return leftBound(nums, target)
+	//return rightBound(nums, target)
 
 }
 
@@ -22,4 +23,22 @@ func leftBound(nums []int, target int) int {
 		}
 	}
 	return left
+}
+
+func rightBound(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			left = mid + 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid
+		}
+	}
+	return right
 }
